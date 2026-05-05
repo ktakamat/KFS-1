da89667a5f9589607a40436357de6975f38d5e10
ktakamat <kkotatakamatsu0126@gmail.com>
2026-05-05 14:34:37 +0200

    change size of 42 and color

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,9 +1,24 @@
+void draw_block(int x, int y, unsigned char color_code) {
+	volatile unsigned short* vga_buffer = (volatile unsigned short*)0xB8000;
+	int index = y * 80 + x;
+	vga_buffer[index] = (unsigned short)((color_code << 4) | color_code) << 8 | ' ';
+}
+
 void kernel_main(void) {
     volatile unsigned short* vga_buffer = (volatile unsigned short*)0xB8000;
+    for (int i = 0; i < 80 * 25; i++) {
+        vga_buffer[i] = (0x00 << 8) | ' ';
+    }
 
-    unsigned short color = 0x0F00;
+    unsigned char cyan = 11;
 
-    vga_buffer[0] = color | '4';
+    for (int y = 5; y <= 7; y++) draw_block(10, y, cyan);
+    draw_block(11, 7, cyan); draw_block(12, 7, cyan);
+    for (int y = 5; y <= 9; y++) draw_block(13, y, cyan);
 
-    vga_buffer[1] = color | '2';
+    draw_block(17, 5, cyan); draw_block(18, 5, cyan); draw_block(19, 5, cyan);
+    draw_block(19, 6, cyan);
+    draw_block(17, 7, cyan); draw_block(18, 7, cyan); draw_block(19, 7, cyan);
+    draw_block(17, 8, cyan);
+    draw_block(17, 9, cyan); draw_block(18, 9, cyan); draw_block(19, 9, cyan);
 }
